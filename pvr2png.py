dfe26cb59e7365c3f9b643f0a32792da4584a837
James Wah <git@james.wah.net.au>
2023-08-17 12:33:54 +1000

    pvr2png: remove numpy dependency
    most of the runtime was just loading numpy

--- pvr2png.py
+++ pvr2png.py
@@ -1,49 +1,52 @@
 #!/usr/bin/env python3
 
-import numpy as np
+import struct
 import png
 import sys
 
-B = [0x55555555, 0x33333333, 0x0F0F0F0F, 0x00FF00FF]
-S = [1, 2, 4, 8]
 def twiddle(x, y):
-    x = (x | (x << S[3])) & B[3]
-    x = (x | (x << S[2])) & B[2]
-    x = (x | (x << S[1])) & B[1]
-    x = (x | (x << S[0])) & B[0]
+    x = (x | (x << 8)) & 0x00FF00FF
+    x = (x | (x << 4)) & 0x0F0F0F0F
+    x = (x | (x << 2)) & 0x33333333
+    x = (x | (x << 1)) & 0x55555555
 
-    y = (y | (y << S[3])) & B[3]
-    y = (y | (y << S[2])) & B[2]
-    y = (y | (y << S[1])) & B[1]
-    y = (y | (y << S[0])) & B[0]
+    y = (y | (y << 8)) & 0x00FF00FF
+    y = (y | (y << 4)) & 0x0F0F0F0F
+    y = (y | (y << 2)) & 0x33333333
+    y = (y | (y << 1)) & 0x55555555
 
     z = x | (y << 1)
 
     return z
 
-if len(sys.argv) != 3:
-    print(f"usage: {sys.argv[0]} infile.pvr outfile.png")
+if len(sys.argv) not in [2, 3]:
+    print(f"usage: {sys.argv[0]} infile.pvr [outfile.png]")
     sys.exit(1)
 
-raw = np.fromfile(sys.argv[1], np.uint16, offset=0x20)
-pixels = np.zeros((512,512), np.uint16)
+if len(sys.argv) == 2:
+    sys.argv.append(sys.argv[1][:-4] + '.png')
+
+with open(sys.argv[1], 'rb') as fp:
+    fp.seek(0x20)
+    raw_bytes = fp.read()
+    raw = struct.unpack(f'<{len(raw_bytes)//2}H', raw_bytes)
+
+image = []
+for row in range(512):
+    image.append([0]*(3*512))
 
 for x in range(512):
     for y in range(512):
         z = twiddle(y, x)
 
         if z < len(raw):
-            pixels[x, y] = raw[z]
+            rawval = raw[z]
+            row = image[y]
+            r = (rawval >> 11) << 3
+            g = ((rawval >> 5) & 0x3f) << 2
+            b = (rawval & 0x1f) << 3
+            row[x*3] = r
+            row[x*3 + 1] = g
+            row[x*3 + 2] = b
 
-pngvals = []
-for y in range(512):
-    row = []
-    pngvals.append(row)
-    for x in range(512):
-        raw = pixels[x, y]
-        r = (raw >> 11) << 3
-        g = ((raw >> 5) & 0x3f) << 2
-        b = (raw & 0x1f) << 3
-        row.extend([r, g, b])
-
-png.from_array(pngvals, 'RGB').save(sys.argv[2])
+png.from_array(image, 'RGB').save(sys.argv[2])
